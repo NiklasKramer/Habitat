b44b9a9197ef19258529b07c52a49d14ee849f2a
Niklas Kramer <43847968+NiklasKramer@users.noreply.github.com>
2021-04-03 19:39:26 +0200

    simple

--- habitat/src/App.js
+++ habitat/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import {Youtube} from "./Youtube.component";
 
 function App() {
   return (
     <div className="App">
-      HALLO
+        <div className='Heading'>{`Welcome To Habitat`}</div>
+        <div>Please make sure to run this on a Mac or Windows device. Supported browsers are Firefox and Chrome.</div>
+            <a target="_blank" href="https://hubs.mozilla.com/VfZdc5z/gaesteliste">Enter</a>
+      <div className={'Element'}><Youtube/></div>
     </div>
   );
 }
---
+++ habitat/src/Youtube.component.js
@@ -0,0 +1,10 @@
+export const Youtube = () => {
+    return(
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/i7aZ2dv8VM0?controls=0"
+                title="YouTube video player" frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen></iframe>
+    )
+
+
+}
