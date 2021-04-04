2b4ec1799213b5ebdcae6353d1e5b09376faba60
Niklas Kramer <43847968+NiklasKramer@users.noreply.github.com>
2021-04-04 15:09:07 +0200

    add content

--- habitat/src/App.js
+++ habitat/src/App.js
@@ -3,22 +3,24 @@ import {Youtube} from "./Youtube.component";
 import {introductions} from "./content";
 import title from './images/title.svg'
 import {Images} from "./Images";
+import logo from './images/LEAVINGvenndiagram.png'
 
 function App() {
     return (
         <div className="App">
-            <img alt={''} className={'Heading'} src={title}></img>
+            <img alt={'header'} className={'Heading'} src={title} />
             <Images/>
             <div className={'text'}>
                 <div>{introductions}</div>
             </div>
             <div className={'Button'}>
-            <a className="MyButton" target="_blank" rel="noreferrer"
-               href={'https://hubs.mozilla.com/VfZdc5z/gaesteliste'}>E N T E R</a>
+                <a className="MyButton" target="_blank" rel="noreferrer"
+                   href={'https://hubs.mozilla.com/VfZdc5z/gaesteliste'}>E N T E R</a>
             </div>
             <div className={'Element'}>
                 <Youtube/>
             </div>
+            <img alt={'logo'} className={'Logo'} src={logo} />
 
         </div>
     );
