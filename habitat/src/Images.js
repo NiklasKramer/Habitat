ccc82528d779a87f02d56879c59b247f514564a2
Niklas Kramer <43847968+NiklasKramer@users.noreply.github.com>
2021-04-05 16:35:38 +0200

    new url

--- habitat/src/Images.js
+++ habitat/src/Images.js
@@ -1,13 +1,14 @@
 import image1 from "./images/image1.jpg";
 import image3 from './images/image3.jpg'
+import {url} from "./content";
 
 export const Images = () => {
     return (
         <div>
-            <a href={'https://hubs.mozilla.com/VfZdc5z/gaesteliste'}>
+            <a href={url}>
                 <img className={'TopImage'} rel="noreferrer" src={image1} alt={''}></img>
             </a>
-            <a href={'https://hubs.mozilla.com/VfZdc5z/gaesteliste'}>
+            <a href={url}>
                 <img className={'TopImage'} rel="noreferrer" src={image3} alt={''}></img>
             </a>
         </div>
